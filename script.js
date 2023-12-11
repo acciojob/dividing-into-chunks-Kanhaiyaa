b8e41fd4c51bac131e2cd277a6c30fe1041c8995
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-11 23:04:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,45 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	const divide=(arr,n) => {
+		let trackingsum=0,ans=[],subArr=[]
+		for (let i = 0; i < arr.length; i++) {
+			if (trackingsum+arr[i] <=n) {
+				subArr.push(arr[i])
+				trackingsum +=arr[i]
+			}
+			else{
+				ans.push(subArr)
+				subArr +=[arr[i]]
+				trackingsum =arr[i]
+			}
+		}
+		ans.push(subArr)
+		return ans
+	}
 };
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
